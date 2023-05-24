f550a41161aa871821ea0c2f2c1fd3683e0078ba
Eliezio Oliveira <eliezio@parolisoft.com>
2023-05-24 12:20:21 +0100

    refactor: Add missing @Inherited to @CatchSlowQueries

--- src/main/java/com/parolisoft/dbquerywatch/junit5/CatchSlowQueries.java
+++ src/main/java/com/parolisoft/dbquerywatch/junit5/CatchSlowQueries.java
@@ -3,6 +3,7 @@ package com.parolisoft.dbquerywatch.junit5;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -13,6 +14,7 @@ import java.lang.annotation.Target;
  */
 @Target({ ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
+@Inherited
 @ExtendWith(CatchSlowQueriesExtension.class)
 public @interface CatchSlowQueries {
 }
