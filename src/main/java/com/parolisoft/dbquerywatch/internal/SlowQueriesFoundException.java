4150a5f24b5c68a2c41bae91dfd4b3a9b14bf47d
Eliezio Oliveira <eliezio@parolisoft.com>
2023-05-26 12:24:27 +0100

    feat: Report datasource info (beanName and jdbcUrl)

--- src/main/java/com/parolisoft/dbquerywatch/internal/SlowQueriesFoundException.java
+++ src/main/java/com/parolisoft/dbquerywatch/internal/SlowQueriesFoundException.java
@@ -1,10 +1,15 @@
 package com.parolisoft.dbquerywatch.internal;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class SlowQueriesFoundException extends CleanRuntimeException {
+
+    private static final String LF_INDENT = "\n    ";
+
     private final List<SlowQueryReport> slowQueries;
 
     public SlowQueriesFoundException(List<SlowQueryReport> slowQueries) {
@@ -22,18 +27,21 @@ public class SlowQueriesFoundException extends CleanRuntimeException {
             SlowQueryReport slowQuery = slowQueries.get(i);
             sb.append('\n')
                 .append(heading("Query %d/%d", i + 1, slowQueries.size()))
-                .append("\nSQL:\n    ")
-                .append(slowQuery.getQuerySql());
-            sb.append("\nExecution Plan:\n    ")
-                .append(slowQuery.getExecutionPlan());
+                .append("\nDataSource:")
+                .append(LF_INDENT).append(slowQuery.getDataSourceName())
+                .append(" (").append(getUrl(slowQuery.getDataSource())).append(')')
+                .append("\nSQL:")
+                .append(LF_INDENT).append(slowQuery.getQuerySql());
+            sb.append("\nExecution Plan:")
+                .append(LF_INDENT).append(slowQuery.getExecutionPlan());
             sb.append("\nIssues:");
             for (Issue issue : slowQuery.getIssues()) {
-                sb.append("\n    - ")
+                sb.append(LF_INDENT).append("- ")
                     .append(issue.toString());
             }
             sb.append("\nCaller Methods:");
             for (String methodName : slowQuery.getMethods()) {
-                sb.append("\n    - ")
+                sb.append(LF_INDENT).append("- ")
                     .append(methodName);
             }
             sb.append('\n');
@@ -49,5 +57,14 @@ public class SlowQueriesFoundException extends CleanRuntimeException {
         int padLen = (80 - 7) - title.length();
         //noinspection MalformedFormatString
         return String.format("%.5s %s %." + padLen + "s", DASHES, title, DASHES);
+    }
+
+    private static String getUrl(DataSource ds) {
+        try {
+            try (Connection connection = ds.getConnection()) {
+                return connection.getMetaData().getURL();
+            }
+        } catch (Exception ignored) {}
+        return "UNKNOWN URL";
     }
 }
