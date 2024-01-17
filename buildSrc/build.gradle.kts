73e2e1a3bfbfd4cd0181afa00c0c9fe7a3c18b13
Eliezio Oliveira <eliezio@pm.me>
2024-01-17 14:18:16 +0000

    chore(buildSrc): Update dependencies
    - Gradle Versions Plugin to 0.50.0
    - Dependency Management Plugin to 1.1.4
    - Commons-Text to 1.11.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,9 +7,9 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.ben-manes", "gradle-versions-plugin", "0.47.0")
+    implementation("com.github.ben-manes", "gradle-versions-plugin", "0.50.0")
     implementation("com.github.gundy", "semver4j", "0.16.4")
     implementation("com.moandjiezana.toml", "toml4j", "0.7.2")
-    implementation("io.spring.gradle", "dependency-management-plugin", "1.1.2")
-    implementation("org.apache.commons", "commons-text", "1.10.0")
+    implementation("io.spring.gradle", "dependency-management-plugin", "1.1.4")
+    implementation("org.apache.commons", "commons-text", "1.11.0")
 }
