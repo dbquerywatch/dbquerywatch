1ec713bc774a521faecdc8f93820987c07c85b8e
Eliezio Oliveira <eliezio@pm.me>
2024-01-23 13:24:58 +0000

    chore(buildSrc): Update dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.ben-manes", "gradle-versions-plugin", "0.50.0")
+    implementation("com.github.ben-manes", "gradle-versions-plugin", "0.51.0")
     implementation("com.github.gundy", "semver4j", "0.16.4")
     implementation("com.moandjiezana.toml", "toml4j", "0.7.2")
     implementation("io.spring.gradle", "dependency-management-plugin", "1.1.4")
