53f6adb2f5b248668f7c80730548c4a979158da8
Eliezio Oliveira <eliezio@pm.me>
2024-09-25 06:30:42 +0100

    chore(build): Upgrade Gradle Plugins
    - ErrorProne 4.1.0
    - GrGit 5.3.0
    - IdeaExt 1.1.10
    - SonarQube 6.0.1.5171

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,10 +24,10 @@ plugins {
     id("com.adarshr.test-logger") version "4.0.0"
     id("com.github.ksoichiro.console.reporter") version "0.6.3"
     id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
-    id("net.ltgt.errorprone") version "4.0.1"
-    id("org.ajoberstar.grgit") version "5.2.2"
-    id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.8"
-    id("org.sonarqube") version "5.1.0.4882"
+    id("net.ltgt.errorprone") version "4.1.0"
+    id("org.ajoberstar.grgit") version "5.3.0"
+    id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.10"
+    id("org.sonarqube") version "6.0.1.5171"
 
     id("dependencyUpdates")
 }
